8a5bef84619df4b0d8c2cb35dcb03b1735ad5767
BuildTools <unconfigured@null.spigotmc.org>
2023-10-21 04:48:10 +1100

    keybind open filetree

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -9,7 +9,10 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
-  config = function ()
-    require('neo-tree').setup {}
+  keys = {
+    { "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "Toggle filetree" },
+  },
+  config = function()
+    require('neo-tree').setup()
   end,
 }
